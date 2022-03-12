c0a5657f0b169dae4b31c1d54b122bb7d9007952
Tiago Rodrigues <tmcrodrigues@gmail.com>
2022-03-12 19:25:07 +0100

    Update CRA

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 import injectAndInitMusickit from './init-musickit'
